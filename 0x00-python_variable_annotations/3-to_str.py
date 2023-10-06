b27b9c878f51a8bedf811ff169fb4183470c2a6a
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-06 02:20:45 +0100

    feat: :sparkles: basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""float to integer"""
+
+
+def to_str(n: float) -> str:
+    """
+        Args:
+            n: float number
+
+        Return:
+            string of type-> Float
+    """
+    return str(n)
