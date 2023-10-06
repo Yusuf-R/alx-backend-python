44eb1a2f6a22c9e6801dd75d92b4e7f0fa9118d9
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-06 02:18:56 +0100

    feat: :sparkles: basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Truncate float to integer"""
+
+
+def floor(n: float) -> int:
+    """
+        Args:
+            n: float number
+        Return:
+            n: truncated to floor
+    """
+    return int(n)
