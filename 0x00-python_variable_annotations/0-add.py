a85241e9148dc060fd3926ab121ef5f4fb0f717f
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-06 02:15:07 +0100

    feat: :sparkles: basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module with typing Python"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        Basic add annotations
+        Args:
+            a: The first parameter (float)
+            b: The second parameter (float)
+
+        Return:
+            Sum of two floats (float)
+    """
+    return (a + b)
