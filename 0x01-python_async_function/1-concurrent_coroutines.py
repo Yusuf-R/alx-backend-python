3a4a362226df49edfaefa3ca378becf5c45a549b
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-20 08:51:47 +0100

    feat: :sparkles: handling multiple concurrnt async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,26 +4,23 @@ import asyncio
 from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-        Args:
-            max_delay: max wait
-            n: spawn function
+    Asynchronously waits for `n` iterations, then returns a list of the delays.
 
-        Return:
-            float time random
+    Args:
+    - n (int): The number of iterations to wait for.
+    - max_delay (int): The maximum delay (in seconds) for each iteration.
+
+    Returns:
+    - A list of the delays (in seconds) for each iteration.
     """
     delays: List[float] = []
-    tasks: List = []
-
     for _ in range(n):
-        tasks.append(wait_random(max_delay))
-
-    for task in asyncio.as_completed((tasks)):
-        delay = await task
+        delay = await wait_random(max_delay)
         delays.append(delay)
-
+    delays.sort()
     return delays
