41c5a2048b301f6b7f97c06c8524df25fb83e638
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-21 05:18:42 +0100

    feat: :sparkles: basics of async generators

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
-""" Basic syntax of await async """
+
+# Write a coroutine called async_generator that takes no arguments.
+
+# The coroutine will loop 10 times, each time asynchronously wait 1 second,
+# then yield a random number between 0 and 10. Use the random module.
+
 import asyncio
 import random
+from typing import Generator
 
 
-async def wait_random(max_delay: int = 10) -> float:
+async def async_generator() -> Generator[float, None, None]:
     """
-        Args:
-            max_delay: max wait
+    Yield a random integer between 0 and 10 asynchronously, 10 times.
 
-        Return:
-            float time random
+    This coroutine function simulates some asynchronous
+    work by sleeping for 1 second between each yield statement.
+
+    Yields:
+      float: a random float number between 0 and 10.
     """
-    delay: float = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-
-    return delay
+    for _ in range(10):
+        await asyncio.sleep(1)  # Simulate some asynchronous work
+        yield random.uniform(0, 10)
