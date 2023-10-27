86155a4636a1119b4c8a4174d026198b009b1ae5
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-28 00:21:03 +0100

    feat: :sparkles: test exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
   def test_access_nested_map(self, nested_map, path, result):
     """ Test access_nested_map """
     self.assertEqual(access_nested_map(nested_map, path), result)
+    
+  @parameterized.expand([
+    ({}, ("a",)),
+    ({"a": 1}, ("a","b"))     
+  ])
+  def test_access_nested_map_exception(self, nested_map, path):
+    """ Test exception access_nested_map """
+    with self.assertRaises(KeyError):
+      access_nested_map(nested_map, path)
 
 
 
