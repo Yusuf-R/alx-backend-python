8f1178185c8b83423b5a8d9b4047556e37b62b52
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-28 01:03:36 +0100

    feat: :sparkles: mocking an HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,9 @@
 
 import unittest
 from parameterized import parameterized
+from unittest.mock import Mock, patch
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
 class TestAccessNestedMap(unittest.TestCase):
   """ Test access_nested_map """
@@ -28,6 +30,22 @@ class TestAccessNestedMap(unittest.TestCase):
     """ Test exception access_nested_map """
     with self.assertRaises(KeyError):
       access_nested_map(nested_map, path)
+      
+class TestGetJson(unittest.TestCase):
+  @parameterized.expand([
+    ("http://example.com", {"payload": True}),
+    ("http://holberton.io", {"payload": False}),
+  ]) 
+  def test_get_json(self, test_url, test_payload):
+    """ Test get_json """
+    with patch('utils.requests.get') as mock_get:
+      mock_get.return_value.ok = True
+      mock_get.return_value.json.return_value = test_payload
+      result = get_json(test_url)
+      self.assertEqual(result, test_payload)
+      mock_get.assert_called_once_with(test_url)    
+  
+
 
 
 
